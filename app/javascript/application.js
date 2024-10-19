99c6bc85032d45a9dc3699cacce9e8612ab67b5d
ciraben <tom.on.github@gmail.com>
2024-10-19 16:26:30 -0700

    Add a button to initiate audio
    - Chrome requires user interaction before autoplay - a button provides that.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+window.addEventListener("DOMContentLoaded", () => {
+  const audio_element = document.querySelector("audio")
+  const button = document.querySelector("button")
+
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    audio_element.play();
+  });
+});
