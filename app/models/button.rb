29424a16b7cffcc32e250fbc23105d396c2e6e2e
ciraben <tom.on.github@gmail.com>
2024-10-19 22:04:23 -0700

    Add database validations

--- app/models/button.rb
+++ app/models/button.rb
@@ -1,4 +1,7 @@
 class Button < ApplicationRecord
   belongs_to :location, class_name: "Room"
   belongs_to :destination, class_name: "Room"
+
+  validates :location, presence: true
+  validates :destination, presence: true
 end
