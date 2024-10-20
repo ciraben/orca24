108452ec4e7240cb13b26d85f10641f51d745144
ciraben <tom.on.github@gmail.com>
2024-10-19 17:38:33 -0700

    Express existing styles via tailwind instead

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        cream: "#e0cb82",
+      }
     },
   },
   plugins: [
