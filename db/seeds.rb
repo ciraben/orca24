1dcc058cb4d9a714ef3865077104599d890b25b5
ciraben <tom.on.github@gmail.com>
2024-10-20 01:54:30 -0700

    Move data into `db/seeds` for easy reload

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+room_data = {
+  "qwench (phone)" => "Qwench!",
+  "street outside qwench - thanku!" => "sure is quiet out here...",
+  "intro" => "This is a cute game i made for orca jam 2024."
+}
+
+button_data = {
+  "go outside" => [
+    "qwench (phone)",
+    "street outside qwench - thanku!"
+  ],
+  "go back inside" => [
+    "street outside qwench - thanku!",
+    "qwench (phone)"
+  ],
+  "start" => [
+    "intro",
+    "qwench (phone)"
+  ]
+}
+
+room_data.each do |label, description|
+  Room.find_or_create_by(label: label) do |room|
+    room.description = description
+  end
+end
+
+button_data.each do |label, rooms|
+  Button.find_or_create_by(label: label) do |button|
+    button.label = label
+    button.location = Room.find_by label: rooms[0]
+    button.destination = Room.find_by label: rooms[1]
+  end
+end
