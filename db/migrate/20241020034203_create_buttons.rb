e3e309ab6dd2fe03b106e4a26baabc5cc8aae518
ciraben <tom.on.github@gmail.com>
2024-10-19 20:51:46 -0700

    Add relations between buttons and rooms
    - see https://stackoverflow.com/questions/2057210/ruby-on-rails-reference-the-same-model-twice

--- db/migrate/20241020034203_create_buttons.rb
+++ db/migrate/20241020034203_create_buttons.rb
@@ -2,8 +2,13 @@ class CreateButtons < ActiveRecord::Migration[7.2]
   def change
     create_table :buttons do |t|
       t.string :label
+      t.references :location
+      t.references :destination
 
       t.timestamps
     end
+    
+    add_foreign_key :buttons, :rooms, column: :location_id, primary_key: :id
+    add_foreign_key :buttons, :rooms, column: :destination_id, primary_key: :id
   end
 end
